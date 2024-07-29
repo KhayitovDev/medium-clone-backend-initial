6c6c8e7c6cf161e739d0110f10547070e020d53c
Khayitboy <khayitovkhayitboy@gmail.com>
2024-07-29 16:02:48 +0500

    static files config

--- core/settings.py
+++ core/settings.py
@@ -110,3 +110,8 @@ STATIC_ROOT = BASE_DIR / "static"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+STATIC_URL ='/static/'
+STATIC_ROOT = BASE_DIR / "static"
+
--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('health/', lambda _: JsonResponse({'detail': 'Healthy'}), name='health'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, docmument_root=settings.STATIC_ROOT)
